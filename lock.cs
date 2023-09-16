c601dfcc00b0e64f2825ea36efc4b3688b021b50
Abir Basumatary <59792752+abir-hub@users.noreply.github.com>
2023-09-16 18:42:45 +0530

    Add files via upload

---
+++ lock.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+
+namespace ThreadStateDemo
+{
+    class Program
+    {
+        static object lockObject = new object();
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(SomeMethod)
+            {
+                Name = "Thread 1"
+            };
+
+            Thread thread2 = new Thread(SomeMethod)
+            {
+                Name = "Thread 2"
+            };
+
+            Thread thread3 = new Thread(SomeMethod)
+            {
+                Name = "Thread 2"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            Console.ReadKey();
+        }
+
+        public static void SomeMethod()
+        {
+            // Locking the Shared Resource for Thread Synchronization
+            lock (lockObject)
+            {
+                Console.Write("[Welcome To The ");
+                Thread.Sleep(1000);
+                Console.WriteLine("World of Dotnet!]");
+            }
+        }
+    }
+}
