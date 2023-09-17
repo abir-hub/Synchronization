fcac11cfb65317b2be67f04090c9b5bbcb63c167
Abir Basumatary <59792752+abir-hub@users.noreply.github.com>
2023-09-18 01:17:33 +0530

    Update lock.cs

--- lock.cs
+++ lock.cs
@@ -66,7 +66,7 @@ namespace ThreadStateDemo
 
             Thread thread3 = new Thread(SomeMethod)
             {
-                Name = "Thread 2"
+                Name = "Thread 3"
             };
 
             thread1.Start();
